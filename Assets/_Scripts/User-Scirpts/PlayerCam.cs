d5cb09e364d39830906e0a22be521a017ef5cdbb
ArtemZ1 <210385@student.glu.nl>
2023-04-26 12:35:56 +0200

    Update
    Shape Shift
    Pick Up
    Human Script

---
+++ Assets/_Scripts/User-Scirpts/PlayerCam.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class PlayerCam : MonoBehaviour
+    {
+           public float mouseSensitivity, yRotLimit;
+           private Vector2 rotation;
+
+           private void Awake()
+           {
+               Cursor.visible = false;
+               Cursor.lockState = CursorLockMode.Locked;
+           }
+
+           void Update()
+        {
+            rotation.x += Input.GetAxis("Mouse X") * mouseSensitivity;
+            rotation.y += Input.GetAxis("Mouse Y") * mouseSensitivity;
+            rotation.y = Mathf.Clamp(rotation.y, -yRotLimit, yRotLimit);
+            var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
+            var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+
+            //Quaternions seem to rotate more consistently than EulerAngles. Sensitivity seemed to change slightly at certain degrees using Euler. transform.localEulerAngles = new Vector3(-rotation.y, rotation.x, 0);
+            transform.localRotation = yQuat/* * xQuat*/; 
+
+            //Rotate the parent y
+            transform.parent.localRotation = xQuat;
+        }
+    }
+}
---
+++ Assets/_Scripts/User-Scirpts/ShapeShift_System.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace _Scripts
+{
+    public class ShapeShift_System : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _listShapeShift;
+        [SerializeField] private List<UnityEvent> _listEvents;
+        [SerializeField] private PickUpScript _pickUpScript;
+        private int choose;
+        
+        private void Update()
+        {
+            if (Input.GetMouseButton(1) && !_pickUpScript.ObjectinHands)
+            {
+                _listShapeShift[3].SetActive(true);
+
+                if (Input.GetKeyDown(KeyCode.Space)) choose++;
+
+
+                switch (choose)
+                {
+                    case 0:
+                        _listShapeShift[0].SetActive(true);
+                        _listShapeShift[1].SetActive(false);
+                        _listShapeShift[2].SetActive(false);
+                        break;
+                    case 1:
+                        _listShapeShift[1].SetActive(true);
+                        _listShapeShift[0].SetActive(false);
+                        _listShapeShift[2].SetActive(false);
+                        break;
+                    case 2:
+                        _listShapeShift[2].SetActive(true);
+                        _listShapeShift[0].SetActive(false);
+                        _listShapeShift[1].SetActive(false);
+                        break;
+                }
+
+                if (choose > 2) choose = 0;
+            }
+            else
+            {
+                _listShapeShift[3].SetActive(false);
+                
+                switch (choose)
+                {
+                    case 0:
+                        _listEvents[0].Invoke();
+                        break;
+                    case 1:
+                        _listEvents[1].Invoke();
+
+                        break;
+                    case 2:
+                        _listEvents[2].Invoke();
+                        break;
+                }
+            }
+        }
+    }
+    
+    
+}
